a538a73eef009bb5cf447f602344ff620190b635
m01nak <m01nak77@gmail.com>
2024-07-04 00:16:00 +0530

    config backup 20240704001600

--- scripts/run-config-backup.sh
+++ scripts/run-config-backup.sh
@@ -76,7 +76,7 @@ cd /Users/moinak/.custom-config
 
 commit_date=$(date +%Y%m%d%H%M%S)
 git add --all
-git commit -m "config backup $commit_date" > /dev/null 2>&1
+git commit -m "config backup $commit_date"
 
 # Push to remote repository
 git push origin main
